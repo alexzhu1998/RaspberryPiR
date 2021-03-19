a809c06b960e9fb9b7d128a32ca78b5bdfef447c
Alex Zhu <codebreaker@Alexs-MacBook-Air.local>
2021-03-19 18:32:43 +1100

    added wiringPiSetup

--- R/fibonacci_main.R
+++ R/fibonacci_main.R
@@ -1,7 +1,7 @@
 # rinternals <- file.path(R.home("include"), "Rinternals.h")
 # file.show(rinternals)
 
-dyn.load(paste0(getwd(),"../src/","fibonacci_main.so"))
+dyn.load(paste0(getwd(),"/../src/","fibonacci_main.so"))
 
 wiringPiSetup <- function() {
   result <- .C("blink", greeting="")
