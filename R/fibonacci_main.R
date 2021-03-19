2530df880a16ae200ecfe1807411ed472cc32915
Alex Zhu <codebreaker@Alexs-MacBook-Air.local>
2021-03-19 18:10:55 +1100

    added wiringPiSetup

--- R/fibonacci_main.R
+++ R/fibonacci_main.R
@@ -2,20 +2,7 @@
 # file.show(rinternals)
 dyn.load("fibonacci_main.so")
 
-x = 1:3
-ret_val = .Call("helloA1")
-
-
-conv <- function(a,b) .Call("conv",a,b)
-
-a= c(1,2,3,4)
-b = c(2,3,4,5)
-conv(a,b)
-# add <- cfunction(c(a = "integer", b = "integer"), "
-#   SEXP result = PROTECT(allocVector(REALSXP, 1));
-#   REAL(result)[0] = asReal(a) + asReal(b);
-#   UNPROTECT(1);
-#
-#   return result;
-# ")
-# add(1, 5)
+wiringPiSetup <- function() {
+  result <- .C("blink", greeting="")
+  return(result$greeting)
+}
--- src/fibonacci_main.cpp
+++ src/fibonacci_main.cpp
@@ -7,7 +7,7 @@ extern "C" {
 #include <Rdefines.h>
 #include <vector>
 
-
+#define ledPin 0
 // int fibonacci_main(int x) {
 //   Fibonacci fib;
 //   return fib.getFibonacciNumber(x);
@@ -19,48 +19,50 @@ extern "C" {
 // }
 
 extern "C" {
-  void wiringPiSetup() {
+  void blink(char **greeting) {
+    *greeting = "Hello World!";
     wiringPiSetup();
+
   }
 }
 
-extern "C" {
-  void addOneToVector(int* n, double* vector) {
-    for (int i = 0; i < *n; ++i)
-      vector[i] += 1.0;
-  }
-}
-extern "C" {
-  SEXP helloA1() {
-    printf("hello World\n");
-    return (R_NilValue);
-  }
-}
-
-extern "C" SEXP conv(SEXP a, SEXP b) {
-  int i, j, na, nb, nab;
-  double *xa, *xb, *xab;
-  SEXP ab;
-  PROTECT(a = AS_NUMERIC(a));
-  PROTECT(b = AS_NUMERIC(b));
-  na = LENGTH(a);
-  nb = LENGTH(b);
-  nab = na +nb -1;
-  PROTECT(ab = NEW_NUMERIC(nab));
-  xa = NUMERIC_POINTER(a);
-  xb = NUMERIC_POINTER(b);
-  xab = NUMERIC_POINTER(ab);
-  for (i = 0; i < nab; i++) {
-    xab[i] = 0.0;
-  }
-  for (i = 0; i < na; i++) {
-    for (j = 0; j < nb; j++) {
-      xab[i+j] += xa[i]*xb[j];
-    }
-  }
-  UNPROTECT(3);
-  return(ab);
-}
+// extern "C" {
+//   void addOneToVector(int* n, double* vector) {
+//     for (int i = 0; i < *n; ++i)
+//       vector[i] += 1.0;
+//   }
+// }
+// extern "C" {
+//   SEXP helloA1() {
+//     printf("hello World\n");
+//     return (R_NilValue);
+//   }
+// }
+//
+// extern "C" SEXP conv(SEXP a, SEXP b) {
+//   int i, j, na, nb, nab;
+//   double *xa, *xb, *xab;
+//   SEXP ab;
+//   PROTECT(a = AS_NUMERIC(a));
+//   PROTECT(b = AS_NUMERIC(b));
+//   na = LENGTH(a);
+//   nb = LENGTH(b);
+//   nab = na +nb -1;
+//   PROTECT(ab = NEW_NUMERIC(nab));
+//   xa = NUMERIC_POINTER(a);
+//   xb = NUMERIC_POINTER(b);
+//   xab = NUMERIC_POINTER(ab);
+//   for (i = 0; i < nab; i++) {
+//     xab[i] = 0.0;
+//   }
+//   for (i = 0; i < na; i++) {
+//     for (j = 0; j < nb; j++) {
+//       xab[i+j] += xa[i]*xb[j];
+//     }
+//   }
+//   UNPROTECT(3);
+//   return(ab);
+// }
 
 // SEXP add(SEXP a, SEXP b) {
 //   SEXP result = PROTECT(allocVector(REALSXP, 1));
