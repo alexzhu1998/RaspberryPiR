1ce9ce5604cfac4a45dd99a27c6f1c0feacaa760
Alex Zhu <codebreaker@alexs-air.home>
2021-04-07 17:31:45 +1000

    hello

--- R/DHT_setup.R
+++ R/DHT_setup.R
@@ -1,7 +1,7 @@
 # rinternals <- file.path(R.home("include"), "Rinternals.h")
 # file.show(rinternals)
 # sourceCpp(paste0(getwd(),"/src/","DHTtempsensor_functions.cpp"))
-dyn.load(paste0(getwd(),"/src/","DHTtempsensor_functions.so"))
+
 
 # DHTInitiate <- function() {
 #     res <- .C("main",out = 0)
@@ -9,9 +9,11 @@ dyn.load(paste0(getwd(),"/src/","DHTtempsensor_functions.so"))
 # }
 
 DHTCFunction <- function() {
+    dyn.load(paste0(getwd(),"/src/","DHTtempsensor_functions.so"))
     res <- .C("myDHT", out = 0)
     return(res$out)
 }
+
 
 # app <- new(DHT)
 # app$humidity
