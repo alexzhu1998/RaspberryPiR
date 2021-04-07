9b6cb6b48c6649aa904e20c9e3c1cabdd23a03b0
Alex Zhu <codebreaker@alexs-air.home>
2021-04-07 17:35:22 +1000

    hello

--- R/DHT_example.R
+++ R/DHT_example.R
@@ -2,8 +2,17 @@ source("R/DHT_setup.R")
 
 # res <- DHTInitiate()
 # print(res)
+count = 0
+store = c()
+while (count < 10) {
+  # print(paste0("Current Counter",count))
+  res <- DHTCFunction()
+  store = c(store,res)
+  # print(paste0("Temperature",res))
+  count = count+ 1
+}
 
-res <- DHTCFunction()
-print(res)
+print(store)
+
 
 
