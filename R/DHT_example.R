694cbacacf33a54b717e9f17bfa82f8b849bff98
Alex Zhu <codebreaker@Alexs-MacBook-Air.local>
2021-04-20 15:44:04 +1000

    hi

--- R/DHT_example.R
+++ R/DHT_example.R
@@ -8,7 +8,7 @@ humid = c()
 for (count in 1:10) {
     print(paste("Current Counter",count))
     res <- DHTCFunction()
-    store = c(store,res$tempOut)
+    temp = c(temp,res$tempOut)
     humid = c(humid,res$humidOut)
     print(paste("Temperature",res$tempOut))
     print(paste("Humidity",res$humidOut))
