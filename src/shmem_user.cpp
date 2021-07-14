da5d9180c0ddd63dec95cecd2765b8b40429c84b
alexzhu1998 <alexzhu1998_aus@hotmail.com>
2021-07-14 20:40:50 +1000

    simplified_template

--- src/shmem_user.cpp
+++ src/shmem_user.cpp
@@ -2,18 +2,13 @@
 #include "DHT11.h"
 #include <time.h> //for time
 #include <unistd.h>
-#include "shared_memory.h"
+#include "shmem/shared_memory.h"
 #include <R.h>
 #include <Rdefines.h>
 #include <R_ext/Utils.h>
 #include <vector>
 #include <chrono> // for measuring real execution time
 #include <thread> // for sleep
-#include <stdlib.h>
-#include <sys/types.h> //key_t
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <R.h>
 
 
 typedef std::chrono::high_resolution_clock::time_point TimeVar;
@@ -26,38 +21,6 @@ typedef std::chrono::high_resolution_clock::time_point TimeVar;
 #define DATE_STRING_SIZE 30
 
 
-// attach a shared memory block, associated with filename, create if doesnt exist
-static int get_shared_block(int size, int id, int random_key) {
-    
-    key_t key = random_key%KEY_RANGE+id;
-    if (key == IPC_RESULT_ERROR) return IPC_RESULT_ERROR;
-
-    return shmget(key,size,IPC_CREAT |0666);
-}
-
-template <typename T>
-void attach_memory_block(my_object<T>* object, int size,int id,int random_key) {
-    int shared_block_id = get_shared_block(size,id, random_key);
-
-    if (shared_block_id == IPC_RESULT_ERROR) perror("IPC_RESULT_ERROR: ");
-
-    object = (my_object<T> *)shmat(shared_block_id, NULL,0);
-    if (object == (void *) IPC_RESULT_ERROR) perror("shmat: ");
-    return;
-}
-
-template <typename T>
-bool detach_memory_block(my_object<T>* block) {
-    return (shmdt(block) != IPC_RESULT_ERROR);
-}
-
-
-bool free_memory_block(int id,int random_key) {
-    int shared_block_id = get_shared_block(NULL_MEMORY,id,random_key);
-
-    if (shared_block_id == IPC_RESULT_ERROR) return NULL;
-    return (shmctl(shared_block_id, IPC_RMID, NULL) != IPC_RESULT_ERROR);
-}
 
 time_t get_raw_time() {
     time_t rawtime;
@@ -123,7 +86,7 @@ void assign_DHT_block(int pin, int sensor ,DHT& dht, my_object<T>* block,int &in
             block->data2 = dht.humidity;
             Rcpp::Rcout << "Humidity is " << block->data2  << "%," << "\t Temperature is " << block->data1  << "*C" << std::endl;
         } else if (sensor == 1) {
-            block->data1 = (T) dht.PhoR_time_to_charge;
+            block->data1 = (T)dht.PhoR_time_to_charge;
             Rcpp::Rcout << "Time to charge is " << block->data1 << " s"<< std::endl;
         }
 
